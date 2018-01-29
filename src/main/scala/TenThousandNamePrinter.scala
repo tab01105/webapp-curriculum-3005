e214fee18667cde14854dbf8e018c3e825762f72
tab01105 <tab01105@gmail.com>
2018-01-29 14:22:18 +0900

    Executorフレームワークの利用

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,10 +1,13 @@
+import java.util.concurrent.{Callable, Executors}
+
 object TenThousandNamePrinter extends App {
-
+  val es = Executors.newFixedThreadPool(10)
   for (i <- 1 to 10000) {
-    new Thread(() => {
-      Thread.sleep(1000)
-      println(Thread.currentThread().getName)
-    }).start()
+    es.submit(new Runnable {
+      override def run(): Unit = {
+        Thread.sleep(1000)
+        println(Thread.currentThread().getName)
+      }
+    })
   }
-
 }
